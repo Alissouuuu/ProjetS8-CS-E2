de0b73819653f4e5edd0f81e75842b745b7cf980
Ima-ne01 <imanedouhi7@gmail.com>
2025-05-26 11:07:21 +0200

    dao actu, event et clubs mises à jours

--- src/main/java/dao/ActuDAO.java
+++ src/main/java/dao/ActuDAO.java
@@ -2,39 +2,61 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.ActualiteClub;
 import util.DBConnexion;
 
 public class ActuDAO {
 
-    public boolean ajouterActualite(ActualiteClub actualite) {
-        String sql = "INSERT INTO actualite_club (titre_actu, titre_conseil, description_actu,description_conseil, titre_competition, date_competition, score_competition,lieu_competition,id_user,id_club) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+	public boolean ajouterActualite(ActualiteClub actualite) {
+		String sql = "INSERT INTO actualite_club (titre_actu, description_actu,id_user,id_club) VALUES (?, ?, ?, ?)";
 
-        try (Connection conn = DBConnexion.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+		try (Connection conn = DBConnexion.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-            stmt.setString(1, actualite.getTitreActu());
-            stmt.setString(2, actualite.getDescriptionActu());
-            stmt.setString(3, null);
-            stmt.setString(4, null);
-            stmt.setString(5, null);
-            stmt.setString(6, null);
-            stmt.setInt(7, 0);
-            if(actualite.getDateCompetition()!=null) {
-                stmt.setDate(8, java.sql.Date.valueOf(actualite.getDateCompetition()));
+			stmt.setString(1, actualite.getTitreActu());
+			stmt.setString(2, actualite.getDescriptionActu());
+			stmt.setInt(3, actualite.getIdUser());
+			stmt.setInt(4, 1);
+			return stmt.executeUpdate() > 0;
 
-            }else
-                stmt.setString(8, "null");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
+	public List<ActualiteClub> listerActualites() throws SQLException {
+		List<ActualiteClub> liste = new ArrayList<>();
+		String sql = "SELECT * FROM actualite_club";
 
-            return stmt.executeUpdate() > 0;
+		try (Connection conn = DBConnexion.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				ResultSet rs = stmt.executeQuery()) {
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+			while (rs.next()) {
+				ActualiteClub a = new ActualiteClub();
+				a.setIdActu(rs.getInt("id_actualite"));
+				a.setTitreActu(rs.getString("titre_actu"));
+				a.setDescriptionActu(rs.getString("description_actu"));
+
+				liste.add(a);
+			}
+		}
+		return liste;
+	}
+	public void supprimerActu(int id) throws SQLException {
+		String sql = "DELETE FROM actualite_club WHERE id_actualite = ?";
+		try (Connection conn = DBConnexion.getConnection();
+		     PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, id);
+			int rowsAffected = stmt.executeUpdate();
+			System.out.println("Nombre de lignes supprimées : " + rowsAffected);
+			
+		}
+	}
+
 }
--- src/main/java/dao/EventDAO.java
+++ src/main/java/dao/EventDAO.java
@@ -5,80 +5,154 @@ import util.DBConnexion;
 
 import java.sql.*;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.servlet.http.HttpSession;
-
 public class EventDAO {
-	
-    
-	
+
 	// créer un nouvel événement
 	public boolean createEvent(Evenement event) throws SQLException {
-	    String sql = "INSERT INTO evenement (nbr_max, date, heure, lieu, description, nom, id_user, id_club) "
-	               + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO evenement (nbr_max, date, heure, lieu, description, nom, id_user, id_club) "
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
-	    try (Connection conn = DBConnexion.getConnection();
-	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
+		try (Connection conn = DBConnexion.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-	        pstmt.setInt(1, event.getNbrMaxParticipants());
-	        pstmt.setDate(2, Date.valueOf(event.getDateEvenement()));
-	        pstmt.setTime(3, Time.valueOf(event.getHeureEvenement()));
-	        pstmt.setString(4, event.getLieuEvenement());
-	        pstmt.setString(5, event.getDescriptionEvenement());
-	        pstmt.setString(6, event.getNomEvenement());
-	        pstmt.setInt(7, 1); 
-	        pstmt.setInt(8, 1); // Remplacez 1 par l'ID du club associé
+			pstmt.setInt(1, event.getNbrMaxParticipants());
+			pstmt.setDate(2, Date.valueOf(event.getDateEvenement()));
+			pstmt.setTime(3, Time.valueOf(event.getHeureEvenement()));
+			pstmt.setString(4, event.getLieuEvenement());
+			pstmt.setString(5, event.getDescriptionEvenement());
+			pstmt.setString(6, event.getNomEvenement());
+			pstmt.setInt(7, 1);
+			pstmt.setInt(8, 1); // Remplacez 1 par l'ID du club associé
 
-	        return pstmt.executeUpdate() > 0;
-	    }
-	}
-	public boolean updateEvent(Evenement event, int eventId) throws SQLException {
-	    String sql = "UPDATE evenement SET nbr_max=?, date=?, heure=?, lieu=?, description=?, nom=? WHERE id=?";
-	    try (Connection conn = DBConnexion.getConnection();
-	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
-	        pstmt.setInt(1, event.getNbrMaxParticipants());
-	        pstmt.setDate(2, Date.valueOf(event.getDateEvenement()));
-	        pstmt.setTime(3, Time.valueOf(event.getHeureEvenement()));
-	        pstmt.setString(4, event.getLieuEvenement());
-	        pstmt.setString(5, event.getDescriptionEvenement());
-	        pstmt.setString(6, event.getNomEvenement());
-	        pstmt.setInt(7, eventId);
-	        return pstmt.executeUpdate() > 0;
-	    }
+			return pstmt.executeUpdate() > 0;
+		}
 	}
 
-	public boolean deleteEvent(int id) throws SQLException {
-	    String sql = "DELETE FROM evenement WHERE id=?";
+	public void updateEvenement(Evenement e) throws SQLException {
+	    String sql = "UPDATE evenement SET nom = ?, date = ?, heure = ?, lieu= ?, nbr_max = ?, description = ? WHERE id_evenement = ?";
 	    try (Connection conn = DBConnexion.getConnection();
-	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
-	        pstmt.setInt(1, id);
-	        return pstmt.executeUpdate() > 0;
+	         PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+	        stmt.setString(1, e.getNomEvenement());
+	        stmt.setDate(2, Date.valueOf(e.getDateEvenement()));
+	        stmt.setTime(3, Time.valueOf(e.getHeureEvenement()));
+	        stmt.setString(4, e.getLieuEvenement());
+	        stmt.setInt(5, e.getNbrMaxParticipants());
+	        stmt.setString(6, e.getDescriptionEvenement());
+	        stmt.setInt(7, e.getIdEvenement());
+
+	        stmt.executeUpdate();
 	    }
+	}
+
+
+	public void supprimerEvenement(int id) throws SQLException {
+		String sql = "DELETE FROM evenement WHERE id_evenement = ?";
+		try (Connection conn = DBConnexion.getConnection();
+		     PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, id);
+			int rowsAffected = stmt.executeUpdate();
+			System.out.println("Nombre de lignes supprimées : " + rowsAffected);
+
+			stmt.executeUpdate();
+			
+		}
 	}
 
 	public List<Evenement> getAllEvents() throws SQLException {
-	    List<Evenement> events = new ArrayList<>();
-	    String sql = "SELECT * FROM evenement";
+		List<Evenement> events = new ArrayList<>();
+		String sql = "SELECT * FROM evenement";
+		try (Connection conn = DBConnexion.getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)) {
+			while (rs.next()) {
+				Evenement e = new Evenement();
+				e.setNomEvenement(rs.getString("nom"));
+				e.setDescriptionEvenement(rs.getString("description"));
+				e.setDateEvenement(rs.getDate("date").toLocalDate());
+				e.setHeureEvenement(rs.getTime("heure").toLocalTime());
+				e.setLieuEvenement(rs.getString("lieu"));
+				e.setNbrMaxParticipants(rs.getInt("nbr_max"));
+				events.add(e);
+			}
+		}
+		return events;
+	}
+	public Evenement getEvenementById(int id) throws SQLException {
+	    String sql = "SELECT * FROM evenement WHERE id_evenement = ?";
 	    try (Connection conn = DBConnexion.getConnection();
-	         Statement stmt = conn.createStatement();
-	         ResultSet rs = stmt.executeQuery(sql)) {
-	        while (rs.next()) {
-	            Evenement e = new Evenement();
-	            e.setNomEvenement(rs.getString("nom"));
-	            e.setDescriptionEvenement(rs.getString("description"));
-	            e.setDateEvenement(rs.getDate("date").toLocalDate());
-	            e.setHeureEvenement(rs.getTime("heure").toLocalTime());
-	            e.setLieuEvenement(rs.getString("lieu"));
-	            e.setNbrMaxParticipants(rs.getInt("nbr_max"));
-	            events.add(e);
+	         PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+	        stmt.setInt(1, id);
+	        try (ResultSet rs = stmt.executeQuery()) {
+	            if (rs.next()) {
+	                Evenement e = new Evenement();
+	                e.setIdEvenement(id);
+	                e.setNomEvenement(rs.getString("nom"));
+	                e.setDescriptionEvenement(rs.getString("description"));
+	                e.setDateEvenement(rs.getDate("date").toLocalDate());
+	                e.setHeureEvenement(rs.getTime("heure").toLocalTime());
+	                e.setLieuEvenement(rs.getString("lieu"));
+	                e.setNbrMaxParticipants(rs.getInt("nbr_max"));
+	                
+	                
+	                return e;
+	            }
 	        }
 	    }
-	    return events;
+	    return null; 
 	}
 
 
+	// Événements passés
+	public List<Evenement> getEvenementsPasses() throws SQLException {
+		List<Evenement> evenements = new ArrayList<>();
+		String sql = "SELECT * FROM evenement WHERE date < ?";
+
+		try (Connection conn = DBConnexion.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+			stmt.setDate(1, Date.valueOf(LocalDate.now()));
+
+			try (ResultSet rs = stmt.executeQuery()) {
+				while (rs.next()) {
+					evenements.add(mapResultSetToEvenement(rs));
+				}
+			}
+		}
+		return evenements;
+	}
+
+	public List<Evenement> getEvenementsAVenir() throws SQLException {
+		List<Evenement> evenements = new ArrayList<>();
+		String sql = "SELECT * FROM evenement WHERE date >= ? ORDER BY date ASC";
+
+		try (Connection conn = DBConnexion.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+			stmt.setDate(1, Date.valueOf(LocalDate.now()));
+
+			try (ResultSet rs = stmt.executeQuery()) {
+				while (rs.next()) {
+					evenements.add(mapResultSetToEvenement(rs));
+				}
+			}
+		}
+		return evenements;
+	}
+
+	private Evenement mapResultSetToEvenement(ResultSet rs) throws SQLException {
+		Evenement e = new Evenement();
+		e.setNomEvenement(rs.getString("nom"));
+		e.setIdEvenement(rs.getInt("id_evenement"));
+		e.setDateEvenement(rs.getDate("date").toLocalDate());
+		e.setHeureEvenement(rs.getTime("heure").toLocalTime());
+		e.setLieuEvenement(rs.getString("lieu"));
+		e.setDescriptionEvenement(rs.getString("description"));
+		e.setNbrMaxParticipants(rs.getInt("nbr_Max"));
+		e.setIdUser(rs.getInt("id_user"));
+		e.setIdClub(rs.getInt("id_club"));
+		return e;
+	}
 
 }
